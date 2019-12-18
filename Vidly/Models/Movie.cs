ff7aa4d53efae3be5afbb919c8537d906264efa2
andre_santos_vixion <andre.santos@vixion.nl>
2019-12-18 21:16:57 +0100

    5. MVC in action

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Vidly.Models
+{
+    /*
+     * Plain old clr
+     * POCO
+     * Sate and behavior of our aplication
+     */
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
