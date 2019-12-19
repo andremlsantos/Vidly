3be0e2b1c146c1ce3cc384f397db3708d4f7a8ed
andre_santos_vixion <andre.santos@vixion.nl>
2019-12-19 16:43:19 +0100

    2. Action Results

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -6,6 +6,11 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
+        /*
+         * ViewResult : ViewResultBase : ActionResult
+         * E considerado boa practica usar ViewResult mas se podermos ter retornar variuos tipos acoes neste metodo
+         * Temos de retornar ActionResult por polimorphismo
+         */
         public ActionResult Random()
         {
             var movie = new Movie()
@@ -14,7 +19,21 @@ namespace Vidly.Controllers
                 Name = "Shrek"
             };
 
-            return View(movie);
+            // to return view
+            //return View(movie);
+
+            // to return simple string
+            //return Content(movie.ToString());
+
+            // to return 404
+            //return HttpNotFound();
+
+            // to return empty page
+            //return new EmptyResult();
+
+            // to return to different controller
+            var argumentsAction = new { page = 1, sortBy = "name" };
+            return RedirectToAction("Index", "Home", argumentsAction);
         }
     }
 }
